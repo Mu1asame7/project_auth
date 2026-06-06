b873876c32fbaed9de62eca4a4398bf017b58c54
Артур Батыршин <Artur20.06.2000@icloud.com>
2026-06-06 12:05:39 +0300

    create service for auth

---
+++ src/service/auth.py
@@ -0,0 +1,31 @@
+from datetime import datetime, timezone, timedelta
+from fastapi import HTTPException, status
+
+import jwt
+from pwdlib import PasswordHash
+
+from src.config import settings
+
+
+class AuthService:
+    password_hash = PasswordHash.recommended()
+
+    @staticmethod
+    def create_access_token(data: dict) -> str:
+        to_encode = data.copy()
+        expire = datetime.now(timezone.utc) + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+        to_encode.update({"exp": expire})
+        encoded_jwt = jwt.encode(to_encode, settings.JWT_SECRET_KEY, algorithm=settings.JWT_ALGORITHM)
+        return encoded_jwt
+
+    def hash_password(self, password: str) -> str:
+        return self.password_hash.hash(password)
+
+    def verify_password(self, password: str, hashed_password: str) -> bool:
+        return self.password_hash.verify(password, hashed_password)
+
+    def encode_token(self, token: str) -> dict:
+        try:
+            return jwt.decode(token, settings.JWT_SECRET_KEY, algorithms=[settings.JWT_ALGORITHM])
+        except jwt.exceptions.DecodeError:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Невалидный токен")
