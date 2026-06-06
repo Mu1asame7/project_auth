b3ccc72adaab6cd13296af89fa8329b8ee7e0c75
Артур Батыршин <Artur20.06.2000@icloud.com>
2026-06-06 12:26:58 +0300

    change requiremets.txt and add router to main

--- src/main.py
+++ src/main.py
@@ -6,8 +6,11 @@ from fastapi import FastAPI
 
 sys.path.append(str(Path(__file__).parent.parent))
 
+from src.api.auth import router as auth_router
+
 app = FastAPI()
 
+app.include_router(auth_router)
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app", reload=True)
