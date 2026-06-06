93cc70ccbbea02895cd95cb89f31a3f9f73474b8
Артур Батыршин <Artur20.06.2000@icloud.com>
2026-06-06 12:51:22 +0300

    add endpoint POST /login

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,7 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response, HTTPException
 
 from src.api.dependencies import DBDep
-from src.schemas.users import UserRequestAdd, UserAdd
+from src.schemas.users import UserRequestAdd, UserAdd, UserLogin
 from src.service.auth import AuthService
 
 router = APIRouter(prefix="/auth", tags=["Авторизация и аутентификация"])
@@ -13,8 +13,33 @@ async def register_user(
         data: UserRequestAdd,
 ):
     hashed_password = AuthService().hash_password(data.password)
-    new_user_data = UserAdd(email=data.email, hashed_password=hashed_password)
+    new_user_data = UserAdd(
+        email=data.email,
+        first_name=data.first_name,
+        last_name=data.last_name,
+        middle_name=data.middle_name,
+        hashed_password=hashed_password
+    )
     await db.users.add(new_user_data)
     await db.commit()
 
     return {"status": "OK"}
+
+
+@router.post("/login")
+async def login_user(
+        db: DBDep,
+        data: UserLogin,
+        response: Response,
+):
+    user = await db.users.get_user_with_hashed_password(email=data.email)
+    if not user:
+        raise HTTPException(status_code=401, detail="User not found")
+    if not AuthService().verify_password(data.password, user.hashed_password):
+        raise HTTPException(status_code=401, detail="Incorrect password")
+    access_token = AuthService().create_access_token({"user_id": user.id})
+    response.set_cookie("access_token", access_token)
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+
--- src/schemas/users.py
+++ src/schemas/users.py
@@ -3,12 +3,23 @@ from pydantic import BaseModel, ConfigDict, EmailStr
 
 class UserRequestAdd(BaseModel):
     email: EmailStr
+    first_name: str
+    last_name: str
+    middle_name: str
     password: str
 
 
 class UserAdd(BaseModel):
     email: EmailStr
+    first_name: str
+    last_name: str
+    middle_name: str
     hashed_password: str
+
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
 
 
 class User(BaseModel):
