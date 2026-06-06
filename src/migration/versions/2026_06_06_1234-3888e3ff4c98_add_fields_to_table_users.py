f1f2ec302c6bf196dce8d7ca0a50f69618679a93
Артур Батыршин <Artur20.06.2000@icloud.com>
2026-06-06 12:37:43 +0300

    add new fields in table "users"

---
+++ src/migration/versions/2026_06_06_1234-3888e3ff4c98_add_fields_to_table_users.py
@@ -0,0 +1,38 @@
+"""add fields to table users
+
+Revision ID: 3888e3ff4c98
+Revises: a678f8ecede3
+Create Date: 2026-06-06 12:34:37.517373
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision: str = "3888e3ff4c98"
+down_revision: Union[str, Sequence[str], None] = "a678f8ecede3"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.add_column("users", sa.Column("last_name", sa.String(), nullable=False))
+    op.add_column(
+        "users", sa.Column("first_name", sa.String(), nullable=False)
+    )
+    op.add_column(
+        "users", sa.Column("middle_name", sa.String(), nullable=True)
+    )
+    op.add_column(
+        "users", sa.Column("is_active", sa.Boolean(), nullable=False)
+    )
+
+
+def downgrade() -> None:
+    op.drop_column("users", "is_active")
+    op.drop_column("users", "middle_name")
+    op.drop_column("users", "first_name")
+    op.drop_column("users", "last_name")
--- src/models/users.py
+++ src/models/users.py
@@ -8,5 +8,9 @@ class UsersORM(Base):
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(primary_key=True)
+    last_name: Mapped[str]
+    first_name: Mapped[str]
+    middle_name: Mapped[str | None]
     email: Mapped[str] = mapped_column(String(200), unique=True)
     hashed_password: Mapped[str] = mapped_column(String(200))
+    is_active: Mapped[bool] = mapped_column(default=True)
